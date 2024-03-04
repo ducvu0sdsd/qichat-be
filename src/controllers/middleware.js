4b6ee3699d7654eb18337acda29dd8badd0d9c92
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2024-03-04 19:26:15 +0700

    upload

--- src/controllers/middleware.js
+++ src/controllers/middleware.js
@@ -6,36 +6,39 @@ const authService = require('../services/auth.service');
 class Middleware {
 
     checkToken = async (req, res, next) => {
-        const accessToken = req.headers.accesstoken
-        const refreshToken = req.headers.refreshtoken
-        const user_id = req.headers.userid
-        const admin = req.headers.admin
-        console.log(user_id, admin, accessToken, refreshToken)
-        if (!user_id || !admin || !refreshToken) {
-            return res.status(500).send('Not Found Information');
-        }
-        jwt.verify(accessToken, process.env.SECRETKEY, (err, decodedAccessToken) => {
-            if (err) {
-                jwt.verify(refreshToken, process.env.SECRETKEY, async (error, decodedRefreshToken) => {
-                    try {
-                        if (error) {
+        try {
+            const accessToken = req.headers.accesstoken
+            const refreshToken = req.headers.refreshtoken
+            const user_id = req.headers.userid
+            const admin = req.headers.admin
+            if (!user_id || !admin || !refreshToken) {
+                return res.status(500).send('Not Found Information');
+            }
+            jwt.verify(accessToken, process.env.SECRETKEY, (err, decodedAccessToken) => {
+                if (err) {
+                    jwt.verify(refreshToken, process.env.SECRETKEY, async (error, decodedRefreshToken) => {
+                        try {
+                            if (error) {
+                                return res.status(500).send('Tokens Expired');
+                            }
+                            const expR = decodedRefreshToken.exp * 1000;
+                            const currentTimestamp = new Date().getTime()
+                            const newTokens = await authService.generateTokens({ user_id, admin }, `${(expR - currentTimestamp) / 1000}s`)
+                            req.tokens = newTokens;
+                            next()
+                        } catch (error) {
                             return res.status(500).send('Tokens Expired');
                         }
-                        const expR = decodedRefreshToken.exp * 1000;
-                        const currentTimestamp = new Date().getTime()
-                        const newTokens = await authService.generateTokens({ user_id, admin }, `${(expR - currentTimestamp) / 1000}s`)
-                        req.tokens = newTokens;
-                        next()
-                    } catch (error) {
-                        return res.status(500).send('Tokens Expired');
-                    }
-                })
-            }
-            else {
-                req.tokens = { accessToken, refreshToken }
-                next()
-            }
-        });
+                    })
+                }
+                else {
+                    req.tokens = { accessToken, refreshToken }
+                    next()
+                }
+            });
+        } catch (error) {
+            console.log(error)
+        }
     }
 
 }
